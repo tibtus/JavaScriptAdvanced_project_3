1a7a6c0ba751793d8b350facc1f3ac1f64bcd16f
TibTus <tibtus@gmail.com>
2021-12-13 18:32:35 +0200

    #project_opp_difference_mod_8

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import MainSlider from "./modules/slider/slider-main";
 import MiniSlider from "./modules/slider/slider-mini";
 import VideoPlayer from "./modules/playVideo";
+import Difference from "./modules/difference";
 
 
 window.addEventListener('DOMContentLoaded', () => {
@@ -39,5 +40,7 @@ window.addEventListener('DOMContentLoaded', () => {
 
     const player = new VideoPlayer('.showup .play', '.overlay');
     player.init();
+
+    new Difference('.officerold', '.officernew', '.officer__card-item').init();
 
 });
