fac535ea029b28c59c36c4ababc879bf9c2db4e9
TibTus <tibtus@gmail.com>
2021-12-15 14:47:09 +0200

    #project_opp_Main-slider-second-page_mod_11

--- src/js/modules/slider/slider-main.js
+++ src/js/modules/slider/slider-main.js
@@ -42,13 +42,7 @@ export default class MainSlider extends Slider {
         this.showSlides(this.slideIndex += n);
     }
 
-    render() {
-    try {
-        try{
-            this.hanson = document.querySelector('.hanson');
-        } catch(e) {}
-        
-
+    bindTriggers() {        
         this.btns.forEach(item => {
             item.addEventListener('click', () => {
                 this.plusSlides(1);
@@ -57,15 +51,35 @@ export default class MainSlider extends Slider {
             item.parentNode.previousElementSibling.addEventListener('click', (e) =>{
                 e.preventDefault();
                 this.slideIndex = 1;
-
-                this.showSlides(this.slideIndex);
-
-                
+                this.showSlides(this.slideIndex);                
             });
         });
 
-        this.showSlides(this.slideIndex);
-    } catch (e) {}    
-        
+        document.querySelectorAll('.prevmodule').forEach(item =>  {
+            item.addEventListener('click', (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(-1);
+            });
+        });
+
+        document.querySelectorAll('.nextmodule').forEach(item =>  {
+            item.addEventListener('click', (e) => {
+                e.stopPropagation();
+                e.preventDefault();
+                this.plusSlides(1);
+            });
+        });
+
+    }
+
+    render() {
+        if (this.container) {
+            try{
+                this.hanson = document.querySelector('.hanson');
+            } catch(e) {} 
+            this.showSlides(this.slideIndex);
+            this.bindTriggers();
+        }         
     }
 }
